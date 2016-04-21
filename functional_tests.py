e0e1c9b0dd9467356280c5642ef03c081aa821f8
mckerley <mckerley@ncsu.edu>
2016-04-20 23:42:17 -0400

    Created unit test case for sample user. WIP.

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,24 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+	def setUp(self):
+		self.browser = webdriver.Firefox()
+		self.browser.implicitly_wait(3)
+
+	def tearDown(self):
+		self.browser.quit()
+
+	def test_can_start_a_list_and_retrieve_it_later(self):
+		#Jim will visit the website and check to see it is a todo site
+		self.browser.get('http://localhost:8000')
+
+		self.assertIn('To-do', self.browser.title)
+		self.fail() #Work still to do
+		#First Jim will add an item to his grocery list
+		#Then Jim will add another item to his list
+		#Then he will note he has a unique url
+		#lastly he will exit
+if __name__ == '__main__':
+	unittest.main(warnings='ignore')
