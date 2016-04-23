41e214cc6ece743d8ec639235c64cd1962721cd7
mckerley <mckerley@ncsu.edu>
2016-04-23 02:59:02 -0400

    Finished Functional Test

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -15,10 +16,27 @@ class NewVisitorTest(unittest.TestCase):
 		self.browser.get('http://localhost:8000')
 
 		self.assertIn('To-Do lists', self.browser.title)
-		self.fail('Finish the test!') #Work still to do
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('To-Do', header_text)
+
 		#First Jim will add an item to his grocery list
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
+
+		inputbox.send_keys('Buy peacock feathers')
+
+		inputbox.send_keys(Keys.ENTER)
+
+		table = self.browser.find_elements_by_tag_name('tr')
+		self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows))
+
+
 		#Then Jim will add another item to his list
+		self.fail('Finish the test!')
+
 		#Then he will note he has a unique url
+
 		#lastly he will exit
+
 if __name__ == '__main__':
 	unittest.main(warnings='ignore')
