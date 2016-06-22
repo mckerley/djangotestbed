8740980b4edf29386d6d3e3763b43d6b9406a9d0
mckerley <mckerley@ncsu.edu>
2016-06-22 04:24:33 -0400

    Refactored the row iteration to reduce duplicate code.

--- functional_tests.py
+++ functional_tests.py
@@ -12,6 +12,11 @@ class NewVisitorTest(unittest.TestCase):
 	def tearDown(self):
 		self.browser.quit()
 
+	def check_for_row_in_list_table(self,row_text):
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text, [row.text for row in rows])
+
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		#Jim will visit the website and check to see it is a todo site
 		self.browser.get('http://localhost:8000')
@@ -25,34 +30,18 @@ class NewVisitorTest(unittest.TestCase):
 		self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
 
 		inputbox.send_keys('Buy peacock feathers')
-
 		inputbox.send_keys(Keys.ENTER)
 
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = self.browser.find_elements_by_tag_name('tr')
-		"""
-        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows),
-			"new todo item did not appear -- its text was:\n%s" % (
-				table.text,
-			)
-		)
-		"""
-
-		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-
-
+		self.check_for_row_in_list_table('1. Buy peacock feathers')
 
 		#Then Jim will add another item to his list
 		inputbox = self.browser.find_element_by_id('id_new_item')
-		self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
-
 		inputbox.send_keys('Use peacock feathers to make a fly')
-
 		inputbox.send_keys(Keys.ENTER)
 
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = self.browser.find_elements_by_tag_name('tr')
-		self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+		#Now Jim checks to see the two items are displayed
+		self.check_for_row_in_list_table('1. Buy peacock feathers')
+		self.check_for_row_in_list_table('2. Use peacock feathers to make a fly')
 
 
 		self.fail('Finish the test!')
