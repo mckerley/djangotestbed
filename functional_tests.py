5ba606b68042160b1c88000454afe1c9a4635729
mckerley <mckerley@ncsu.edu>
2016-06-22 03:15:58 -0400

    Removed the db file.

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -29,10 +30,31 @@ class NewVisitorTest(unittest.TestCase):
 
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = self.browser.find_elements_by_tag_name('tr')
-		self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows),"new todo item did not appear")
+		"""
+        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows),
+			"new todo item did not appear -- its text was:\n%s" % (
+				table.text,
+			)
+		)
+		"""
+
+		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+
 
 
 		#Then Jim will add another item to his list
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
+
+		inputbox.send_keys('Use peacock feathers to make a fly')
+
+		inputbox.send_keys(Keys.ENTER)
+
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = self.browser.find_elements_by_tag_name('tr')
+		self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+
+
 		self.fail('Finish the test!')
 
 		#Then he will note he has a unique url
