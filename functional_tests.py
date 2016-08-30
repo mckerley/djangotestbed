c16d2e3a2a23c25959e92a0284cbe8a3fe6a1be3
mckerley <mckerley@ncsu.edu>
2016-08-29 20:01:03 -0400

    Model for list items and fixed minor issue with text assertions in functionaltests

--- functional_tests.py
+++ functional_tests.py
@@ -5,50 +5,50 @@ import unittest
 
 class NewVisitorTest(unittest.TestCase):
 
-	def setUp(self):
-		self.browser = webdriver.Firefox()
-		self.browser.implicitly_wait(3)
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
 
-	def tearDown(self):
-		self.browser.quit()
+    def tearDown(self):
+        self.browser.quit()
 
-	def check_for_row_in_list_table(self,row_text):
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn(row_text, [row.text for row in rows])
+    def check_for_row_in_list_table(self,row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
 
-	def test_can_start_a_list_and_retrieve_it_later(self):
-		#Jim will visit the website and check to see it is a todo site
-		self.browser.get('http://localhost:8000')
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        #Jim will visit the website and check to see it is a todo site
+        self.browser.get('http://localhost:8000')
 
-		self.assertIn('To-Do lists', self.browser.title)
-		header_text = self.browser.find_element_by_tag_name('h1').text
-		self.assertIn('To-Do', header_text)
+        self.assertIn('To-Do lists', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
-		#First Jim will add an item to his grocery list
-		inputbox = self.browser.find_element_by_id('id_new_item')
-		self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
+        #First Jim will add an item to his grocery list
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
 
-		inputbox.send_keys('Buy peacock feathers')
-		inputbox.send_keys(Keys.ENTER)
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
 
-		self.check_for_row_in_list_table('1. Buy peacock feathers')
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
-		#Then Jim will add another item to his list
-		inputbox = self.browser.find_element_by_id('id_new_item')
-		inputbox.send_keys('Use peacock feathers to make a fly')
-		inputbox.send_keys(Keys.ENTER)
+        #Then Jim will add another item to his list
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
 
-		#Now Jim checks to see the two items are displayed
-		self.check_for_row_in_list_table('1. Buy peacock feathers')
-		self.check_for_row_in_list_table('2. Use peacock feathers to make a fly')
+        #Now Jim checks to see the two items are displayed
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
 
-		self.fail('Finish the test!')
+        self.fail('Finish the test!')
 
-		#Then he will note he has a unique url
+        #Then he will note he has a unique url
 
-		#lastly he will exit
+        #lastly he will exit
 
 if __name__ == '__main__':
-	unittest.main(warnings='ignore')
+    unittest.main(warnings='ignore')
